b0e00f2a8cfbaedf68b84fcffc33a6eff9167d91
Pratham <pratham.tikkisetty@gmail.com>
2023-07-29 11:04:54 +0530

    database models created

--- models.py
+++ models.py
@@ -1,11 +1,21 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from database import Base
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey,DATE,LargeBinary,Float
 
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:1234@127.0.0.1:3306/BookStore'
+class Book(Base):
+    __tablename__ = "books"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    author = Column(String, index=True)
+    published_date = Column(String)
+    isbn_number = Column(String, unique=True, index=True)
+    price = Column(Float)
+    
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
+class Rating(Base):
+    __tablename__ = "ratings"
+    id = Column(Integer, primary_key=True, index=True)
+    book_id = Column(Integer, ForeignKey("books.id"))
+    user_name = Column(String, index=True)
+    rating = Column(Integer)
+    review = Column(String)
+    
